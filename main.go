7189a074a6232fe80a6de4c6d56f405cbf7c6923
mishimabd <meirkhanabdullayev@yahoo.com>
2022-10-25 20:17:27 +0600

    commit

--- main.go
+++ main.go
@@ -2,13 +2,27 @@ package main
 
 import "fmt"
 
-func main() {
-	array := []int{1, 2, 3, 4, 5}
-	for i := 0; i < len(array); i++ {
-		if (array[i] % 2) == 0 {
-			fmt.Print(array[i])
-			fmt.Println(" It's even number")
-		}
+type Nigger struct {
+	price int
+	name  string
+}
 
+func (n *Nigger) getName() {
+	fmt.Println(n.name)
+}
+
+func (an *Nigger) getPrice() {
+	fmt.Println(an.price)
+}
+
+func main() {
+	n := Nigger{
+		name:  "Kaley",
+		price: 124,
 	}
+	an := Nigger{
+		price: 500,
+		name:  "Zoki",
+	}
+	fmt.Println(an, n)
 }
