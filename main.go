84b90027e8b2ca8c55b8a58a7ea299caac73a602
mishimabd <meirkhanabdullayev@yahoo.com>
2022-10-29 01:14:24 +0600

    working with goroutines

--- main.go
+++ main.go
@@ -2,71 +2,28 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
-//type Nigger struct {
-//	price int
-//	name  string
-//}
-
-//
-//func (n *Nigger) NotEating() {
-//	fmt.Println("I am not eating")
-//}
-//
-//func (n *Nigger) EatChicken() {
-//	fmt.Println("I am eating chicken!")
-//}
-//
-//type Acts interface {
-//	Working()
-//	NotEating()
-//	EatChicken()
-//}
-//
-//func NewModel(b int, arg string) *Acts {
-//	return &Nigger{}
-//}
-//
-//func (n *Nigger) Working() {
-//	fmt.Println("I'm damn")
-//}
-
-//func (n *Nigger) getName() {
-//	fmt.Println(n.name)
-//}
-//
-//func (an *Nigger) getPrice() {
-//	fmt.Println(an.price)
-//}
-
-//func onedelay() {
-//	time.Sleep(time.Second * 3)
-//	fmt.Println("I am nigger 3")
-//}
-//func second() {
-//	time.Sleep(time.Second * 4)
-//	fmt.Println("I am nigger 4")
-//}
-//func third() {
-//	time.Sleep(time.Second * 5)
-//	fmt.Println("I am nigger 5")
-//}
-//func left() {
-//	for {
-//		fmt.Println("I am nigger which is leftover")
-//		time.Sleep(time.Second * 1)
-//	}
-//}
-
 func main() {
-	fmt.Print("Hello ")
+	waitingroup := sync.WaitGroup{}
+	waitingroup.Add(4)
 	go func() {
-		fmt.Print("World ")
+		waitingroup.Done()
+		fmt.Print("1 ")
 	}()
-	fmt.Println("!")
+	go func() {
+		waitingroup.Done()
+		fmt.Print("2 ")
+	}()
+	go func() {
+		waitingroup.Done()
+		fmt.Print("3 ")
+	}()
+	go func() {
+		waitingroup.Done()
+		fmt.Print("4 ")
+	}()
+	waitingroup.Wait()
+	fmt.Println("Exit!")
 }
-
-//func Anything(anything interface{}) {
-//	fmt.Println(anything)
-//}
