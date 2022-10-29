bf456daa1586ac3e0d5cacba3ea02d4463b1d7c8
mishimabd <meirkhanabdullayev@yahoo.com>
2022-10-30 00:52:53 +0600

    commit

--- main.go
+++ main.go
@@ -1,31 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-	"time"
-)
-
 func main() {
-	waitingroup := sync.WaitGroup{}
-	waitingroup.Add(4)
-	go func() {
-		time.Sleep(time.Second * 5)
-		waitingroup.Done()
-		fmt.Print("1 ")
-	}()
-	go func() {
-		waitingroup.Done()
-		fmt.Print("2 ")
-	}()
-	go func() {
-		waitingroup.Done()
-		fmt.Print("3 ")
-	}()
-	go func() {
-		waitingroup.Done()
-		fmt.Print("4 ")
-	}()
-	waitingroup.Wait()
-	fmt.Println("Exit!")
+	//HellO! Here i am just solving tasks for Stepik course.
 }
