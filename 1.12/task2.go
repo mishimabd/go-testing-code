00093ad0de46dfad99b243a6abdefe2e7b84651f
mishimabd <meirkhanabdullayev@yahoo.com>
2022-11-05 23:51:01 +0600

    another day

--- 1.12/task2.go
+++ 1.12/task2.go
@@ -1,1 +1,13 @@
 package main
+
+import "fmt"
+
+func main() {
+	var number int
+	var array [10]int
+	fmt.Scan(&number)
+	for i := 0; i < number; i++ {
+		fmt.Scan(&array[i])
+	}
+	fmt.Println(array[3])
+}
--- 1.12/task3.go
+++ 1.12/task3.go
@@ -1,1 +1,21 @@
 package main
+
+import "fmt"
+
+func main() {
+	var mass [5]int
+	for i := 0; i < len(mass); i++ {
+		fmt.Scan(&mass[i])
+	}
+	fmt.Println(findMax(mass))
+}
+
+func findMax(arr [5]int) int {
+	max := arr[0]
+	for i := 0; i < len(arr); i++ {
+		if arr[i] > max {
+			max = arr[i]
+		}
+	}
+	return max
+}
